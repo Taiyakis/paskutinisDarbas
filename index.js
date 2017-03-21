0e22570dda374efbf16b9ae5d09d0a2ab4cdc82a
Taiyakis <bc924c@gmail.com>
2017-03-21 23:11:18 +0200

    RESTful

--- index.js
+++ index.js
@@ -1,25 +1,172 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
+var mysql = require('mysql');
+var fs = require('fs');
 
-app.disable('x-powered-by');
+//kintamieji
+
+var vardas, pastas, slaptas, who;
+var tema;
+var destytojas;
+var klausimaS;
+var pamNr;
 
 app.use(express.static('public'))
+app.use(bodyParser());
+app.use(bodyParser.json());
+app.use('/', router);
 
+var connection;
 
-app.get('/', function(request, response) {
-	var blocks = 'Testas';
-	response.send(blocks);
+app.get("/pamokos", function(req,res) {
+	pamokos(res);
+});
+app.get("/vartotojai", function(req,res) {
+	tikrinti(res);
 });
 
-app.listen(8001, function() {
-	console.log('Listening server on port 8001');
+app.get("/login", function(req,res) {
+	res.redirect('/login.html');
 });
 
-app.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!")
-})
+app.post("/register", function(req,res) {
+	who = 2;
+	console.log("Role = " + who);
+	pamokos(res);
+});
+app.post("/pridetiPamoka", function(req,res) {
+	res.redirect('/pridetipam.html');
+});
+app.post("/irasytiPam", function(req,res) {
+	pamNr = req.body.pamokosNr;
+	klausimaS = req.body.klausimas;
+	destytojas = req.body.destytojas;
+	tema = req.body.tema;
+	console.log(pamNr + " " + klausimaS + " " + destytojas + " " + tema);
+	pridetiKlausima(res);
+	res.redirect('/home.html');
+});
 
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+app.post("/registerpart2", function(req,res) {
+	vardas = req.body.username;
+	pastas = req.body.email;
+	slaptas = req.body.passwords;
+	console.log(vardas + " " + pastas + " " + slaptas);
+	registruotis(res);
+	res.redirect('/registerdone.html');
+});
+
+function registruotis(res){
+	
+	connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'vartotojasdb'
+	});
+	connection.connect(function(error) {
+		if(!!error) {
+			console.log('Error');
+		} else {
+			console.log('Connected');
+			var post = {
+				name: vardas,
+				email: pastas,
+				passwords: slaptas,
+				role: who
+			};
+			var query = connection.query('INSERT INTO vartotojai SET ?', post, function(err, result) {
+				
+			});
+			query.sql;
+			connection.end();
+		}
+	});
+}
+function pridetiKlausima(res){
+	connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'vartotojasdb'
+	});
+	connection.connect(function(error) {
+		if(!!error) {
+			console.log('Error');
+		} else {
+			console.log('Connected');
+			var post = {
+				pamokosNr: pamNr,
+				klausimas: klausimaS,
+				autorius: destytojas,
+				tema: tema
+			};
+			var query = connection.query('INSERT INTO pamokostable SET ?', post, function(err, result) {
+				if(!!error) {
+					consoloe.log('Irasymo klaida klausimo');
+				} else {
+					query.sql;
+				}
+			});
+			connection.end();
+		}
+	});
+}
+function tikrinti(res){
+	connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'vartotojasdb'
+	});
+	connection.connect(function(error) {
+		if(!!error) {
+			console.log('Error');
+		} else {
+			console.log('Connected');
+			connection.query("SELECT * FROM vartotojai", function(error, rows, fields) {
+			if(!!error) {
+				console.log('Error in the query');
+			} else {
+				console.log(rows);
+				res.json(rows);
+			}
+			});
+			connection.end();
+		}
+	});
+}
+
+function onlyUnique(value, index, self) {
+	return self.indexOf(value) == index;
+}
+
+function pamokos(res){
+	connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'vartotojasdb'
+	});
+	connection.connect(function(error) {
+		if(!!error) {
+			console.log('Error');
+		} else {
+			console.log('Connected');
+			connection.query("SELECT * FROM pamokostable", function(error, rows, fields) {
+			if(!!error) {
+				console.log('Error in the query');
+			} else {
+				console.log(rows);
+				res.json(rows);
+			}
+			});
+			connection.end();
+		}
+	});
+}
+
+
+app.listen(3000);
